045bb95c20aba9d654c89a6ab41b5cf33d550cf9
Matt Cook <matt@studiosapiens.io>
2025-08-10 17:18:13 -0500

    Bump version to 1.4.0

--- src/index.ts
+++ src/index.ts
@@ -260,7 +260,7 @@ class ConfigExplorer {
 program
   .name('cfg')
   .description('Config file explorer and analyzer')
-  .version('1.3.0');
+  .version('1.4.0');
 
 // Initialize config file
 program
